7564ebae338dc55e96e4d3e977159b3e07e41ee9
Nguyễn Hoàng Vũ <hoangvu22062001@gmail.com>
2024-02-22 14:22:01 +0700

    Make Navbar and create components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App bg-slate-100">
-      App
+    <div className="">
+      <Navbar />
+      <div>
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/DetailSong.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DetailSong() {
+  return (
+    <div>DetailSong</div>
+  )
+}
+
+export default DetailSong
